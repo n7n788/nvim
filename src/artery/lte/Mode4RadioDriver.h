e18b0c5955e4879a0e2699ee6450fe6a00eb6148
Brian McCarthy <brianmccarthy95@gmail.com>
2019-03-11 15:07:24 +0000

    Added the necessary mode 4 integration capabilities

---
+++ src/artery/lte/Mode4RadioDriver.h
@@ -0,0 +1,35 @@
+#ifndef Mode4RADIODRIVER_H_ZJ0SI5XC
+#define Mode4RADIODRIVER_H_ZJ0SI5XC
+
+#include "artery/nic/ChannelLoadMeasurements.h"
+#include "artery/nic/RadioDriverBase.h"
+#include <omnetpp/clistener.h>
+#include <omnetpp/csimplemodule.h>
+
+namespace artery
+{
+
+class Mode4RadioDriver : public RadioDriverBase, public omnetpp::cListener
+{
+	public:
+		void initialize() override;
+		void handleMessage(omnetpp::cMessage*) override;
+
+	protected:
+		void handleDataIndication(omnetpp::cMessage*);
+		void handleDataRequest(omnetpp::cMessage*) override;
+		void receiveSignal(omnetpp::cComponent*, omnetpp::simsignal_t, bool, omnetpp::cObject*) override;
+
+	private:
+		omnetpp::cModule* mHost = nullptr;
+		omnetpp::cGate* mLowerLayerOut = nullptr;
+		omnetpp::cGate* mLowerLayerIn = nullptr;
+		omnetpp::cMessage* mChannelLoadReport = nullptr;
+		omnetpp::simtime_t mChannelLoadReportInterval;
+		ChannelLoadMeasurements mChannelLoadMeasurements;
+};
+
+} // namespace artery
+
+#endif /* Mode4RADIODRIVER_H_ZJ0SI5XC */
+
